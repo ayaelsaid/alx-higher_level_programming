e869024bb7b3094cbd5ce74f40bffa34f30bc38c
ayaelsaid <ayae7675@gmail.com>
2023-11-27 19:00:15 -0800

    my

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,11 +1,16 @@
 #include "lists.h"
 #include <stdio.h>
+/**
+ * check_cycle - ...
+ * @list: ...
+ * Return: ...
+*/
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
 	listint_t *faster = list;
 
-	while (slow && faster & faster->next)
+	while (slow && faster && faster->next)
 	{
 		slow = slow->next;
 		faster = faster->next->next;
