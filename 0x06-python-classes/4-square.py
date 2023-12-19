ead1ea5247a680c06c5252fe8148ebb40f6fd336
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2023-12-19 20:01:21 +0200

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""
+without importing any module
+"""
+
 
 class Square:
-
+    """
+    Empty class that define a square by Private instance attribute size,
+    instantiation with size
+    """
     def __init__(self, size=0):
+        """
+        size : no type/value verification
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -10,14 +20,23 @@ class Square:
         self.__size = size
 
     def area(self):
+        """
+        Public instance that returns the current square area
+        """
         return self.__size ** 2
 
     @property
     def size(self):
+        """
+        retrieve instance attribute
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        set instance attribute
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
