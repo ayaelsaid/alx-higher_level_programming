6fe9349f79107daa1e88a448845f8eea3c44fcc3
ayaelsaid <ayae7675@gmail.com>
2023-12-18 22:02:59 -0800

    my python classes2

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""
+no module imported
+"""
+
+
 class Square:
+    """
+
+    Empty class that defines a square
+    """
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+without importoing any module
+"""
+
+
+class Square:
+    """
+
+    Empty class that defines a square
+    """
+
+    pass
+
+    def __init__(self, size):
+        """
+
+        size : no type/value verification
+        """
+        self.__size = size
