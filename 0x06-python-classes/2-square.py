b78d0fa61dc3537993fcc21f989c13b593b2e969
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2023-12-19 19:51:57 +0200

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""
+without importing any module
+"""
+
+
 class Square:
+    """
+    Empty class that define a square by Private instance attribute size,
+    instantiation with size
+    """
 
     def __init__(self, size=0):
+        """
+        size : no type/value verification
+        """
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
