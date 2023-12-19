32b55e6726c4222e0390d928e88547147676045d
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2023-12-19 19:56:33 +0200

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""
+without importing any module
+"""
+
+
 class Square:
+    """
+    Empty class that define a square by Private instance attribute size,
+    instantiation with size
+    """
 
     def __init__(self, size=0):
+        """
+        size : no type/value verification
+        """
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -9,5 +21,8 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """
+        Public instance that returns the current square area
+        """
         self.area = self.__size ** 2
         return (self.area)
