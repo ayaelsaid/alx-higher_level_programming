f874b601e2c57e10525cd9fcc58cfe2f8a30bb48
ayaelsaid <ayae7675@gmail.com>
2024-02-03 20:46:11 -0800

    infinite

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ != "__main__":
+    exit()
+
+if len(sys.argv) == 1:
+    print("0")
+else:
+    result = 0
+    for i in sys.argv[1:]:
+        result += int(i)
+    print("{}".format(result))
