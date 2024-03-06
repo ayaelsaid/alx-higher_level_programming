754ac081974b2f1bf1638cf3d53faafa9fc3f1d8
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-06 05:02:34 +0200

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ score INT
 );
 INSERT INTO second_table (id, name, score)
 VALUE
-(id = 1, name = “John”, score = 10),
-(id = 2, name = “Alex”, score = 3),
-(id = 3, name = “Bob”, score = 14),
-(id = 4, name = “George”, score = 8);
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
