d9acffc226e6ca154cc7c5fc1fd30ed52280990b
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-08 21:48:27 +0200

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,8 @@
 -- that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
 SELECT tv_genres.name
 FROM tv_genres
-RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter" AND tv_show_genres.show_id IS NULL
+WHERE tv_genres.name NOT IN (SELECT tv_genres.name 
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE title = "Dexter")
 ORDER BY tv_genres.name;
