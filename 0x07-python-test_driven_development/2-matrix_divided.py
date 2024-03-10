4ebce802e5097dc1f763d153c4a3f5a8b258a2ec
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-10 02:55:23 +0200

    Create 2-matrix_divided.txt

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,21 +10,28 @@ def matrix_divided(matrix, div):
         new_marrix
         """
     if div == 0:
-        raise ZeroDivisionError("the message division by zero")
+        raise ZeroDivisionError("division by zero")
+
     if not isinstance(div, (int, float)):
-       raise TypeError("the message div must be a number")
-    new_matrix=[]
+        raise TypeError("div must be a number")
+    new_matrix = []
+
     if not all(len(row) == len(matrix[0])for row in matrix):
         raise ValueError("Each row of the matrix must be of the same size")
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError("Each element in the matrix must be a number")
-
     for row in matrix:
-       if not all(isinstance(coulmn, (int, float)) for coulmn in row):
-           raise TypeError("Each element in the matrix must be a number")
-        new_row=[]
+        if not all(isinstance(coulmn, (int, float)) for coulmn in row):
+            raise TypeError("Each element in the matrix must be a number")
+    for row in matrix:
+        new_row = []
         for coulmn in row:
-            new_colm = round(coulmn / div,2)
+            new_colm = round(coulmn / div, 2)
             new_row.append(new_colm)
         new_matrix.append(new_row)
     return new_matrix
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
