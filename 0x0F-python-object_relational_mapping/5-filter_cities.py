1fef934f05e97633ec8d74130d4b1504ee17c430
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-18 03:38:48 +0200

    Create 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+# code should not be executed when imported
+if __name__ == '__main__':
+
+    # make a connection
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    state_name = argv[4]
+    # Create a cursor object
+    cursor = db.cursor()
+    # execute sql query
+    cursor.execute("SELECT cities.name \
+      FROM cities \
+      INNER JOIN states ON cities.state_id = states.id \
+      WHERE name = %s \
+      ORDER BY cities.id ASC", (state_name,))
+    # Fetch all rows
+    allrows = cursor.fetchall()
+    # display rows
+    for row in allrows:
+        print(row)
+    # close all
+    cursor.close()
+    db.close()
