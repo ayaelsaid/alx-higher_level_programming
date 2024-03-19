0485f1f31392ac3e76a94dca5a4f040de860e2fe
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-19 04:09:51 +0200

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -21,8 +21,7 @@ if __name__ == '__main__':
     # Fetch all rows
     allrows = cursor.fetchall()
     # display rows
-    for row in allrows:
-        print(", ".join(row), end="")
+    print(", ".join(for row in allrows), end="")
     # close all
     cursor.close()
     db.close()
